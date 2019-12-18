ebe45c788408cb770590b0c69821cc74a8c684ee
dsterentyev <52151786+dsterentyev@users.noreply.github.com>
2019-12-18 18:31:57 +0300

    Update mt_sync.conf.pl

--- mt_sync.conf.pl
+++ mt_sync.conf.pl
@@ -4,27 +4,36 @@
 $master_ip = '10.32.255.13';                    # ip
 $master_ssh_login = 'conf_sync';                # login
 $master_ssh_password = 'some-P@sS\/\/0Rcl';     # password    
-$master_ssh_port = 52222;                       # ssh port
+$master_ssh_port = 22;                       # ssh port
 $master_ssh_args = '';                          # additional parameters for ssh
 
 #slave router params
 $slave_ip = '10.32.255.14';                     # ip
 $slave_ssh_login = 'conf_sync';                 # login
 $slave_ssh_password = 'some-P@sS\/\/0Rcl';      # password
-$slave_ssh_port = 52222;                        # ssh port
+$slave_ssh_port = 22;                        # ssh port
 $slave_ssh_args = '';                           # additional parameters for ssh
 
 #configuration statements with comment included this line will be ignored
-$protective_comment = '[nosync]';               
+$protective_comment = '[nosync]';
 
 #configuration statements started with it will be ignored
 %branches_ignored = (
     'interface ethernet' => 1,
     'interface vrrp' => 1,
-    'system identity' => 1,
+    'interface bridge' => 1,
     'tool' => 1,
     'user' => 1,
-    'system' => 1
+    'queue interface' => 1,
+    'system' => 1,
+    'port' => 1,
+    'snmp' => 1
+);
+
+#override ingnores list
+%branches_force_included = (
+    'system scheduler' => 1,
+    'system script' => 1
 );
 
 #configuration branches in which order of statements will be preserved
@@ -32,7 +41,13 @@ $protective_comment = '[nosync]';
     'ip firewall filter' => 1,
     'ip firewall nat' => 1,
     'ip firewall mangle' => 1,
-    'ip route rule' => 1
+    'ip route rule' => 1,
+    'caps-man access-list' => 1,
+    'interface wireless channels' => 1,
+    'interface wireless access-list' => 1,
+    'interface bridge port' => 1,
+    'interface bridge filter' => 1,
+    'interface bridge nat' => 1,
     #need populate all such branches
 );
 
